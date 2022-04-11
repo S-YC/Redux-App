d4af3227dd3bff381e8e2cb43b9cfc7784aec6e7
S-YC <s9292909@gmail.com>
2022-04-11 23:59:48 +0900

    connect -> Hooks 사용하여 컨테이너 컴포넌트 적용

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,21 +1,25 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
 import { decrease, increase } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+    // state 상태 조회
+    const number = useSelector(state => state.counter.number);
+    // Dispatch 사용을 위한 useDispatch Hook 선언 
+    const dispatch = useDispatch();
+
+    // useCallback 최적화
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDncrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+        <Counter
+            number={number}
+            onIncrease={onIncrease}
+            onDecrease={onDncrease}
+        />
     );
 };
 
-// 소스 최적화
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    {
-        increase,
-        decrease,
-    },
-)(CounterContainer);
+export default CounterContainer;
